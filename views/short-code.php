dcbe1b7e77f17dc5baeda1ffe1e2c5196c68701d
austinankur <austin.ankur@ithands.net>
2016-07-04 15:07:32 +0530

    Only logged in users can see ops portal interface

--- views/short-code.php
+++ views/short-code.php
@@ -1,19 +1,26 @@
 <?php
-if (!empty($base_url)) {
-    ?>
-    <!--  Ops Portal Start  -->
-    <div appdev-opsportal="default" portal-theme="<?php echo $theme ?>"></div>
-    <script
-        async="async"
-        defer="defer"
-        type="text/javascript"
-        src="<?php echo esc_url($base_url) ?>steal/steal.js?OpsPortal&ver=<?php echo urlencode(WPOP_PLUGIN_VER) ?>"
-        config="<?php echo esc_url($base_url) ?>stealconfig.js">
-    </script>
-    <!-- Ops Portal Ends -->
-    <?php
+//only visible to logged in users
+if (is_user_logged_in()) {
+    if (!empty($base_url)) {
+        ?>
+        <!--  Ops Portal Start  -->
+        <div appdev-opsportal="default" portal-theme="<?php echo $theme ?>"></div>
+        <script
+            async="async"
+            defer="defer"
+            type="text/javascript"
+            src="<?php echo esc_url($base_url) ?>steal/steal.js?OpsPortal&ver=<?php echo urlencode(WPOP_PLUGIN_VER) ?>"
+            config="<?php echo esc_url($base_url) ?>stealconfig.js">
+        </script>
+        <!-- Ops Portal Ends -->
+        <?php
+    } else {
+        ?>
+        <!-- Ops Portal not configured properly -->
+        <?php
+    }
 } else {
     ?>
-    <!-- Ops Portal not configured properly -->
+    <!-- Ops Portal - You are not logged in to WordPress-->
     <?php
-} ?>
+}

bc834368345a66e18769abff93a87e13f6dbb185
austinankur <austin.ankur@ithands.net>
2016-07-05 10:56:47 +0530

    Fix session creation in ops-portal

--- views/short-code.php
+++ views/short-code.php
@@ -1,14 +1,13 @@
 <?php
 //only visible to logged in users
-//todo also check if cookie exists ?
+//todo also check if ticket cookie exists ?
 if (is_user_logged_in()) {
     if (!empty($base_url)) {
         ?>
         <!--  Ops Portal Start  -->
         <div appdev-opsportal="default" portal-theme="<?php echo $theme ?>"></div>
+        <script type="text/javascript" src="<?php echo esc_url($base_url) ?>begin"></script>
         <script
-            async="async"
-            defer="defer"
             type="text/javascript"
             src="<?php echo esc_url($base_url) ?>steal/steal.js?OpsPortal&ver=<?php echo urlencode(WPOP_PLUGIN_VER) ?>"
             config="<?php echo esc_url($base_url) ?>stealconfig.js">
